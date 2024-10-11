366b4372495a46c2df0fee693e5f67f8cfd8e01b
datawater <86855332+datawater@users.noreply.github.com>
2024-10-12 00:58:28 +0400

    chore: removed unnecessary code from main

--- src/main.c
+++ src/main.c
@@ -1,26 +1,13 @@
 #include "utils.h"
 #include "board.h"
-#include <signal.h>
-
-static void handle_abort(int sig) {
-    UNUSED(sig);
-    printf("CAUGHT SIGABORT\n");
-
-    exit(1);
-}
 
 int main(int argc, char** argv) {
-    signal(SIGABRT, handle_abort);
 
     UNUSED(argc);
     UNUSED(argv);
 
     bitboard_t board = 0x55aa55aa55aa55aa;
     printf("%s\n", bitboard_to_debug_string(board));
-
-    printf("raising SIGABRT\n");
-    fflush(stdout);
-    abort();
 
     return 0;
 }
